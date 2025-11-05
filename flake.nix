9977f703200dd557f3a9e4f358de3a4a9b1edc14
Armleth <armand.thibaudon@epita.fr>
2025-11-05 22:47:12 +0100

    fix(nix): flake is now good

--- flake.nix
+++ flake.nix
@@ -1,23 +1,40 @@
 {
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils, }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        rs-python = pkgs.python312.withPackages (ps:
-          with ps; [
+        rs-python = pkgs.python312.withPackages (
+          ps: with ps; [
             numpy
+            keras
             tensorflow
             tensorflow-datasets
             matplotlib
+            ipywidgets
 
             notebook
             ipykernel
-          ]);
-      in with pkgs; {
-        devShells.default =
-          mkShell { buildInputs = [ rs-python jupyter-all ]; };
-      });
+          ]
+        );
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rs-python
+            jupyter-all
+          ];
+        };
+      }
+    );
 }
-
